42758908583952b49e83f995e7da2d8a3a9a3d27
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-12-19 18:48:21 +0300

    production build

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,30 +1,39 @@
 import http from 'http';
 import moment from 'moment';
 
-import {gauge, registry} from './prometheus';
-import {request} from './request';
+import { gauge, registry } from './prometheus';
+import { request } from './request';
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer(async (_req, res) => {
-  const status = await request('status');
-  const net_info = await request('net_info');
-  const m = await registry.metrics();
+  let latest_block_height = -1;
+  let desync = -1;
+  let n_peers = -1;
 
-  const data = status.data.result.sync_info;
-  const latest_block_height = Number(data.latest_block_height);
+  try {
+    const status = await request('status');
+    const net_info = await request('net_info');
 
-  const now = moment().unix();
-  const latest_block_time = moment(data.latest_block_time).unix();
-  const desync = now - latest_block_time;
 
-  const n_peers = Number(net_info.data.result.n_peers);
+    const data = status.data.result.sync_info;
+
+    const now = moment().unix();
+    const latest_block_time = moment(data.latest_block_time).unix();
+
+    desync = now - latest_block_time;
+    n_peers = Number(net_info.data.result.n_peers);
+    latest_block_height = Number(data.latest_block_height);
+  } catch (e: any) {
+    console.log(e.code);
+  }
 
   gauge.latest_block_height.set(latest_block_height);
   gauge.n_peers.set(n_peers);
   gauge.desync.set(desync);
 
+  const m = await registry.metrics();
   res.statusCode = 200;
   res.end(m);
 });
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-## Installation
+## Ansible Installation
+https://github.com/cosmos/testnets/tree/master/public
+https://github.com/hyphacoop/cosmos-ansible
+
+
+## Manual Installation
 
 see https://github.com/cosmos/testnets/tree/master/public
 
 theta-testnet-001
-
 git checkout v7.0.0
 
 make install
@@ -12,7 +16,6 @@ gaiad init CUSTOM_MONIKER --chain-id theta-testnet-001
 wget https://github.com/cosmos/testnets/raw/master/public/genesis.json.gz
 gunzip genesis.json.gz
 cp genesis.json $HOME/.gaia/config/genesis.json
-
 
 cd $HOME/.gaia/config
 sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.001uatom"/' app.toml
@@ -25,9 +28,11 @@ gaiad start --x-crisis-skip-assert-invariants
 ## Testnet
 https://explorer.theta-testnet.polypore.xyz/
 
+## App 
+http://127.0.0.1:3000/
+
 
 ## API
-
-https://docs.tendermint.com/v0.34/rpc/#/Info/health
-
-http://localhost:26657/status?
+https://docs.tendermint.com/v0.34/rpc/#/
+http://localhost:26657/status
+http://localhost:26657/net_info
