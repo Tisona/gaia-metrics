090bf7db11ce7425e635200e706e5faf90ac5e22
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2022-12-23 19:14:29 +0300

    gaia - prometheus metrics collection made in go p2

--- app/go/metrics/collect.go
+++ app/go/metrics/collect.go
@@ -5,10 +5,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/tidwall/gjson"
 )
 
-func collect(metrics string) string {
-	resp, err := http.Get(fmt.Sprintf("http://localhost:26657/%s", metrics))
+const GAIA_HOST = "http://localhost:26657"
+
+func readAPI(resource string) string {
+	resp, err := http.Get(fmt.Sprintf("%s/%s", GAIA_HOST, resource))
 
 	if err != nil {
 		log.Print(err)
@@ -16,7 +21,7 @@ func collect(metrics string) string {
 		return string("")
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, _ := ioutil.ReadAll(resp.Body)
 
 	return string(body)
 }
@@ -26,14 +31,23 @@ func CollectMetrics() (float64, float64, float64) {
 	var desync float64 = -1
 	var n_peers float64 = -1
 
-	status := collect("status")
-	net_info := collect("net_info")
+	net_info := readAPI("net_info")
+	status := readAPI("status")
 
-	if status != "" {
-
-	}
 	if net_info != "" {
+		n_peers = gjson.Get(net_info, "result.n_peers").Float()
 
+		log.Printf(fmt.Sprintf("peers: %f", n_peers))
+	}
+	if status != "" {
+		latest_block_height = gjson.Get(status, "result.sync_info.latest_block_height").Float()
+		latest_block_time := gjson.Get(status, "result.sync_info.latest_block_time")
+
+		timeGaia, _ := time.Parse(time.RFC3339Nano, latest_block_time.Str)
+		desync = float64(time.Now().Unix() - timeGaia.Unix())
+
+		log.Printf(fmt.Sprintf("latest_block_height: %f", latest_block_height))
+		log.Printf(fmt.Sprintf("desync: %f", desync))
 	}
 
 	return latest_block_height, desync, n_peers
